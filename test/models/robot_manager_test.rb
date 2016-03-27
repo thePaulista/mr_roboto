9aaf513a091aeeba0e7782985f5a2d2b7f56e657
Hedy <hedywoo@gmail.com>
2016-03-26 22:06:56 -0600

    finished crud test

--- test/models/robot_manager_test.rb
+++ test/models/robot_manager_test.rb
@@ -8,7 +8,7 @@ class RobotManagerTest < Minitest::Test
 
     assert_equal 0, zero.count
 
-   create_robot(1)
+    create_robot(1)
 
    robot = robot_manager.all.last
 
@@ -39,20 +39,37 @@ class RobotManagerTest < Minitest::Test
    assert_equal "#{robot.department}", robot.department
   end
 
-
   def test_it_can_update_a_robot
-  skip
-   create_robots(3)
+    zero = robot_manager.all
+    assert_equal 0, zero.count
 
+    create_robot(1)
 
+    robot_id = robot_manager.all.last
+    robot = robot_manager.find(robot_id)
+    assert_equal 3, robot.id
+    assert_equal "#{robot.name}", robot.name
+    assert_equal "#{robot.city}", robot.city
+    assert_equal "#{robot.state}", robot.state
+    assert_equal "#{robot.avatar}", robot.avatar
+    assert_equal "#{robot.birthdate}", robot.birthdate
+    assert_equal "#{robot.date_hired}", robot.date_hired
+    assert_equal "#{robot.department}", robot.department
   end
 
   def test_it_can_delete_a_robot
-    skip
+    zero = robot_manager.all
+    assert_equal 0, zero.count
 
-    create_robots(3)
+    create_robot(3)
 
+    robot_id = robot_manager.all.last
+    robot = robot_manager.find(robot_id)
+    assert_equal 3, robot.id
+    assert_equal "#{robot.name}", robot.name
+    assert_equal "#{robot.city}", robot.city
 
-
+    deleted = robot_manager.delete(robot_id)
+    refute deleted, robot_manager.all
   end
 end
